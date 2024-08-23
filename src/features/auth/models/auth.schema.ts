e58ec3b9a760ba3ce45990c7fafa9b69c5ae9484
oladapo <oladapoelijah49@gmail.com>
2024-08-23 15:39:07 +0100

    Adding chat feature

--- src/features/auth/models/auth.schema.ts
+++ src/features/auth/models/auth.schema.ts
@@ -16,6 +16,7 @@ const authSchema: Schema = new Schema(
     passwordResetExpires: { type: Number }
   },
   {
+    // This is used to delete the password if the document is called
     toJSON: {
       transform(_doc, ret) {
         delete ret.password;
@@ -25,17 +26,20 @@ const authSchema: Schema = new Schema(
   }
 );
 
+// This method will be used if the document is created for the first time
 authSchema.pre('save', async function (this: IAuthDocument, next: () => void) {
   const hashedPassword: string = await hash(this.password as string, SALT_ROUND);
   this.password = hashedPassword;
   next();
 });
 
+// This method is used to compare the password against the current password
 authSchema.methods.comparePassword = async function (password: string): Promise<boolean> {
   const hashedPassword: string = (this as unknown as IAuthDocument).password!;
   return compare(password, hashedPassword);
 };
 
+// Use this method when the user is performing changed password or user is updating their password
 authSchema.methods.hashPassword = async function (password: string): Promise<string> {
   return hash(password, SALT_ROUND);
 };
