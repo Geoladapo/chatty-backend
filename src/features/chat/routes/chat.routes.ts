1199066aca3d1a31e52e69372873e1f57fb6b39e
oladapo <oladapoelijah49@gmail.com>
2024-09-10 02:37:12 +0100

    Get Conversation List from db

--- src/features/chat/routes/chat.routes.ts
+++ src/features/chat/routes/chat.routes.ts
@@ -1,6 +1,7 @@
 import { authMiddleware } from '@/root/shared/globals/helpers/auth-middleware';
 import express, { Router } from 'express';
 import { Add } from '../controllers/add-chat-message';
+import { Get } from '../controllers/get-chat-messages';
 
 class ChatRoutes {
   private router: Router;
@@ -10,9 +11,10 @@ class ChatRoutes {
   }
 
   public routes(): Router {
+    this.router.get('/chat/message/conversation-list', authMiddleware.checkAuthentication, Get.prototype.conversationList);
     this.router.post('/chat/message', authMiddleware.checkAuthentication, Add.prototype.meesage);
     this.router.post('/chat/message/add-chat-users', authMiddleware.checkAuthentication, Add.prototype.addChatUsers);
-    this.router.post('/chat/rempve-chat-users', authMiddleware.checkAuthentication, Add.prototype.removeChatUsers);
+    this.router.post('/chat/remove-chat-users', authMiddleware.checkAuthentication, Add.prototype.removeChatUsers);
     return this.router;
   }
 }
