f7870eaaa0663fe7ea9db6c34a5d0db49cae22db
oladapo <oladapoelijah49@gmail.com>
2024-09-13 02:50:05 +0100

    Add user map inside users socketIO handler and emit users online

--- src/shared/sockets/chat.ts
+++ src/shared/sockets/chat.ts
@@ -1,5 +1,6 @@
 import { ISenderReceiver } from '@/chat/interfaces/chat.interfaces';
 import { Server, Socket } from 'socket.io';
+import { connectedUsersMap } from './user';
 
 export let socketIOChatObject: Server;
 
@@ -14,11 +15,11 @@ export class SocketIOChatHandler {
   public listen(): void {
     this.io.on('connection', (socket: Socket) => {
       socket.on('join room', (users: ISenderReceiver) => {
-        // const { senderName, receiverName } = users;
-        // const senderSocketId: string = connectedUsersMap.get(senderName) as string;
-        // const receiverSocketId: string = connectedUsersMap.get(receiverName) as string;
-        // socket.join(senderSocketId);
-        // socket.join(receiverSocketId);
+        const { senderName, receiverName } = users;
+        const senderSocketId: string = connectedUsersMap.get(senderName) as string;
+        const receiverSocketId: string = connectedUsersMap.get(receiverName) as string;
+        socket.join(senderSocketId);
+        socket.join(receiverSocketId);
       });
     });
   }
