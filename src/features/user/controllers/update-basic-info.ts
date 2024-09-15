31d171c952b79b542e3e6b82cfb037a6d6c7d479
oladapo <oladapoelijah49@gmail.com>
2024-09-15 02:53:20 +0100

    update info and social link implemeted

---
+++ src/features/user/controllers/update-basic-info.ts
@@ -0,0 +1,32 @@
+import { joiValidation } from '@/root/shared/globals/decorators/joi-validation.decorators';
+import { UserCache } from '@/service/redis/user.cache';
+import { Request, Response } from 'express';
+import HTTP_STATUS from 'http-status-codes';
+import { basicInfoSchema, socialLinksSchema } from '../schemes/info';
+import { userQueue } from '@/service/queues/user.queue';
+
+const userCache: UserCache = new UserCache();
+
+export class Edit {
+  @joiValidation(basicInfoSchema)
+  public async info(req: Request, res: Response): Promise<void> {
+    for (const [key, value] of Object.entries(req.body)) {
+      await userCache.updateSingleUserItemInCache(`${req.currentUser!.userId}`, key, `${value}`);
+    }
+    userQueue.addUserJob('updateBasicInfoInDB', {
+      key: `${req.currentUser!.userId}`,
+      value: req.body
+    });
+    res.status(HTTP_STATUS.OK).json({ message: 'Updated successfully' });
+  }
+
+  @joiValidation(socialLinksSchema)
+  public async social(req: Request, res: Response): Promise<void> {
+    await userCache.updateSingleUserItemInCache(`${req.currentUser!.userId}`, 'social', req.body);
+    userQueue.addUserJob('updateSocialLinksInDB', {
+      key: `${req.currentUser!.userId}`,
+      value: req.body
+    });
+    res.status(HTTP_STATUS.OK).json({ message: 'Updated successfully' });
+  }
+}
